ab19babd1d18a8760b7526e350d5571b95d2cebb
cbrockenbrough21 <cqt5de@virginia.edu>
2025-01-21 19:38:33 -0500

    setup for ConvertToVect

---
+++ Convert/ConvertToVect/setup.sh
@@ -0,0 +1,5 @@
+source /project/ptgroup/spinquest/this-e1039.sh
+
+export LD_LIBRARY_PATH=$DIR_INST/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/project/ptgroup/Catherine/Fun4AllVectFileManager/Fun4AllVectFileManager/inst/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/project/ptgroup/spinquest/core/pr.136/lib:$LD_LIBRARY_PATH
