52867f38755e0eb7386e91e0b9a39b2cfd78d297
cbrockenbrough21 <cqt5de@virginia.edu>
2025-02-12 11:40:40 -0500

    updated setup script

--- Test/Main/GetEvents/setup.sh
+++ Test/Main/GetEvents/setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Source the spinquest environment
-source /project/ptgroup/spinquest/this-e1039.sh
 
 # Load the Python module
+module purge
+module load miniforge
+module load gcc/11.4.0
+module load openmpi/4.1.4
+module load root/6.32.06
 module load intel-compilers/2023.1.0
 module load impi/2021.9.0
-module load python/3.9.16
+module load python/3.11.4
